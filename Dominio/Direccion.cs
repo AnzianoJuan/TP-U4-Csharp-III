b3c239d50ddb0bf4ba2462e9ba7c8d297798c3fc
Juan anziano <juanianziano22@gmail.com>
2025-01-15 18:47:12 -0300

    primer commit , no esta terminada la maqueta solo la clase de dominio y un poco del proyecto de presentacion donde tengo los web forms

---
+++ Dominio/Direccion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+    }
+
+    public class DireccionNegocio
+    {
+        public List<Direccion> listar()
+        {
+
+            List<Direccion> listaDeDirecciones = new List<Direccion>();
+
+            listaDeDirecciones.Add(new Direccion());
+            listaDeDirecciones.Add(new Direccion());
+
+            listaDeDirecciones[0].Id = 1;
+            listaDeDirecciones[0].Calle = "Colon y bs as";
+            listaDeDirecciones[0].Altura = 589;
+
+            listaDeDirecciones[1].Id = 2;
+            listaDeDirecciones[1].Calle = "Leloir 653";
+            listaDeDirecciones[1].Altura = 653;
+
+            return listaDeDirecciones;
+
+        }
+
+    }
+}
---
+++ Presentacion/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+
+namespace Presentacion
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["ListaDirecciones"] == null)
+            {
+                DireccionNegocio negocio = new DireccionNegocio();
+                Session.Add("ListaDirecciones", negocio.listar());
+            }
+
+            DataGridViewDireccion.DataSource = Session["ListaAutos"];
+            DataGridViewDireccion.DataBind();
+        }
+    }
+}
